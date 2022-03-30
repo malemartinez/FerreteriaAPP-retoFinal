b7863db016a927d9f3d94a1c13946bbe7251d105
Alejandra Martinez <martinezmayra605@gmail.com>
2022-03-30 14:13:28 -0500

    Adicion de archivos en java

---
+++ src/main/java/com/Ferreteria/RetoFinal/Model/Proveedor.java
@@ -0,0 +1,45 @@
+package com.Ferreteria.RetoFinal.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document()
+public class Proveedor {
+
+    @Id
+    private String id = UUID.randomUUID().toString().substring(0, 10);
+
+    private String nombre;
+    private String celular;
+    private String documentoIdentidad;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+}
---
+++ src/test/java/com/Ferreteria/RetoFinal/RetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ferreteria.RetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
