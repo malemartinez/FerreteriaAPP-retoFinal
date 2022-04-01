f0fe10728c47ebe29efee1475b44e05833f5294d
Alejandra Martinez <martinezmayra605@gmail.com>
2022-04-01 15:26:33 -0500

    adicion de dependencia lombok y adicion de controlador para factura

---
+++ src/main/java/com/Ferreteria/RetoFinal/Controller/FacturaController.java
@@ -0,0 +1,58 @@
+package com.Ferreteria.RetoFinal.Controller;
+
+import com.Ferreteria.RetoFinal.Model.DTO.FacturaDTO;
+import com.Ferreteria.RetoFinal.Model.DTO.VolanteDTO;
+import com.Ferreteria.RetoFinal.Model.Factura;
+import com.Ferreteria.RetoFinal.Model.Volante;
+import com.Ferreteria.RetoFinal.services.FacturaServices;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FacturaController {
+
+    @Autowired
+    FacturaServices facturaServices;
+    ModelMapper mapper;
+
+    @PostMapping("/facturas")
+    @ResponseStatus(HttpStatus.CREATED)
+    private Mono<FacturaDTO> save(@RequestBody FacturaDTO facturaDTO) {
+        return this.facturaServices.save(facturaDTO)
+                .flatMap( volante -> Mono.just(mapper.map(volante, FacturaDTO.class)));
+    }
+
+    @GetMapping("/facturas")
+    private Flux<Factura> findAll() {
+        return this.facturaServices.findAll();
+    }
+
+    @GetMapping("/facturas/{id}")
+    private Mono<ResponseEntity<FacturaDTO>> findById(@PathVariable("id") String id) {
+        return this.facturaServices.findById(id)
+                .flatMap(factura -> Mono.just(ResponseEntity.ok(factura)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+
+    }
+
+    @DeleteMapping("/facturas/{id}")
+    private Mono<ResponseEntity<FacturaDTO>> delete(@PathVariable("id") String id) {
+        return this.facturaServices.delete(id)
+                .flatMap(factura -> Mono.just(ResponseEntity.ok(factura)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+
+    }
+
+    @PutMapping("/facturas/{id}")
+    private Mono<ResponseEntity<FacturaDTO>> update(@PathVariable("id") String id, @RequestBody FacturaDTO facturaDTO) {
+        return this.facturaServices.update(id, facturaDTO)
+                .flatMap(factura -> Mono.just(ResponseEntity.ok(factura)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+
+    }
+}
