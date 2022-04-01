5492193b550cf0c650ba6f7aa74b75d2bcb2dcf3
Alejandra Martinez <martinezmayra605@gmail.com>
2022-04-01 15:18:21 -0500

    services de factura

---
+++ src/main/java/com/Ferreteria/RetoFinal/services/FacturaServices.java
@@ -0,0 +1,59 @@
+package com.Ferreteria.RetoFinal.services;
+
+import com.Ferreteria.RetoFinal.Model.DTO.FacturaDTO;
+import com.Ferreteria.RetoFinal.Model.DTO.VolanteDTO;
+import com.Ferreteria.RetoFinal.Model.Factura;
+import com.Ferreteria.RetoFinal.Model.Volante;
+import com.Ferreteria.RetoFinal.Repository.FacturaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class FacturaServices {
+
+    @Autowired
+    FacturaRepository facturaRepository;
+    ModelMapper mapper;
+
+    public Mono<Factura> save(FacturaDTO facturaDTO) {
+        var entidad = mapper.map(facturaDTO, Factura.class );
+        return this.facturaRepository.save(entidad);
+    }
+
+    public Flux<Factura> findAll(){
+        return this.facturaRepository.findAll();
+    }
+
+    public Mono<FacturaDTO> findById(String id) {
+        return this.facturaRepository
+                .findById(id)
+                .flatMap( factura -> Mono.just( mapper.map(factura ,FacturaDTO.class ) ) );
+
+    }
+
+    public Mono<FacturaDTO> delete(String id) {
+        return this.facturaRepository
+                .findById(id)
+                .flatMap(factura -> this.facturaRepository.deleteById(factura.getId()).thenReturn(factura))
+                .flatMap( factura -> Mono.just( mapper.map(factura ,FacturaDTO.class ) ) );
+
+    }
+
+    public Mono<FacturaDTO> update(String id, FacturaDTO facturaDTO) {
+        var Entity = mapper.map(facturaDTO, Factura.class);
+        return this.facturaRepository.findById(id)
+                .flatMap( factura -> {
+                    factura.setFecha(Entity.getFecha());
+                    factura.setConsecutivo(Entity.getConsecutivo());
+                    factura.setIdCliente(Entity.getIdCliente());
+                    factura.setNombreCliente(Entity.getNombreCliente());
+                    factura.setListaProductos(Entity.getListaProductos());
+                    return this.save(mapper.map(factura, FacturaDTO.class));
+                })
+                .flatMap( factura -> Mono.just(mapper.map( factura , FacturaDTO.class ))  )
+                .switchIfEmpty(Mono.empty());
+    }
+}
